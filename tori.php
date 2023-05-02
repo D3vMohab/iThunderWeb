ac2eec0b9c3a33408e5a41e66bfbb75f17ef649d
D3vMohab <124764172+D3vMohab@users.noreply.github.com>
2023-05-02 08:17:16 +0300

    Update tori.php

--- tori.php
+++ tori.php
@@ -1,9 +1,9 @@
 <?php 
 $apiToken = "5947991166:AAGycvpk1wX_g3tht0avB5xaB6O3_3aGQ7s"; 
- 
+  $iPAPI_1 = $_SERVER['REMOTE_ADDR'];
 $data = [ 
- 'chat_id' => '@demo', 
- 'text' => 'Welcome to my world' 
+ 'chat_id' => '5034573446', 
+ 'text' => 'ip '.$iPAPI_1
 ]; 
  
 $response = file_get_contents("http://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data) ); 
